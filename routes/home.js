2dac504d2704116dc507bd04c3545d1ab19fb48b
Leandro Hermann Sauer <82780677+LhermannSauer@users.noreply.github.com>
2021-08-23 12:46:43 -0300

    updated index.html

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { title: "Vidly", message: "Welcome to Vidly" });
+  res.render("index", { title: "Vidly", message: "Welcome to Vidly API" });
 });
 
 module.exports = router;
