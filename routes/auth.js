a51f28c274e813ed558606ee1a33891559f6dcdd
Leandro Hermann Sauer <82780677+LhermannSauer@users.noreply.github.com>
2021-08-23 16:53:24 -0300

    fixed a bug in the auth route that prevented users from logging in

--- routes/auth.js
+++ routes/auth.js
@@ -13,22 +13,18 @@ function validateAuth(req) {
     password: Joi.string().min(5).max(255).required(),
   };
 
-  router.post("/", validate(validateAuth), async (req, res) => {
-    let user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send("Invalid email or password.");
-
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    if (!validPassword)
-      return res.status(400).send("Invalid email or password.");
-
-    const token = user.generateAuthToken();
-    res.send(token);
-  });
-
   return Joi.validate(req, schema);
 }
+
+router.post("/", validate(validateAuth), async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
+
+  const token = user.generateAuthToken();
+  res.send(token);
+});
 
 module.exports = router;
